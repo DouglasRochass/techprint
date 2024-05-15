55efc048d8bf3fbf5ebde52e0553441cb918140d
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2024-05-15 14:34:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,8 @@ const pedidos = require('./routers/pedidos')
 
 const  app = express()
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 
-};
 
-app.use(cors(corsOptions));
+app.use(cors();
 app.use(express.json());
 
 
