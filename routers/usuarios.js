7188ae44da74d234c4189591dca7adc49345cfc9
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2024-05-28 13:42:12 -0300

    Update usuarios.js

--- routers/usuarios.js
+++ routers/usuarios.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../controller/usuarios');
 const authgestor = require('../middleware/auth')
 
-router.get('/usuarios', userController.getAllUsers);
+router.get('/usuarios', authgestor, userController.getAllUsers);
 router.delete('/usuarios/:email', authgestor, userController.deleteUser)
 module.exports = router;
