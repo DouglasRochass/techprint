6ab3e9882ff5d3a1c31444dbc30c3b16e9c7b262
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2024-06-03 16:28:55 -0300

    Update usuarios.js

--- models/usuarios.js
+++ models/usuarios.js
@@ -1,9 +1,47 @@
-const { Sequelize, DataTypes } = require('sequelize');
+// const { Sequelize, DataTypes } = require('sequelize');
+// const sequelize = require('../db/conexao');
+
+
+
+// const usuario = sequelize.define('Usuario', {
+//   nome: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   email:{
+//     type: DataTypes.STRING,
+//     unique: true,
+//     allowNull: false
+//   },
+//   senha: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   cargo:{
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   turma: {
+//     type: DataTypes.STRING
+//   }
+// },
+// {
+//   // Para não criar a coluna createdAt e updateAt do sequelize e 
+//   // também para não pluralizar a tabela 
+//   timestamps: false,
+//   freezeTableName: true
+// });
+
+
+// usuario.sync();
+
+// module.exports = usuario;
+
+const { DataTypes } = require('sequelize');
 const sequelize = require('../db/conexao');
+const Pedidos = require('./pedidos'); // Importe o modelo Pedidos aqui
 
-
-
-const usuario = sequelize.define('Usuario', {
+const Usuario = sequelize.define('Usuario', {
   nome: {
     type: DataTypes.STRING,
     allowNull: false,
@@ -24,15 +62,12 @@ const usuario = sequelize.define('Usuario', {
   turma: {
     type: DataTypes.STRING
   }
-},
-{
-  // Para não criar a coluna createdAt e updateAt do sequelize e 
-  // também para não pluralizar a tabela 
+}, {
   timestamps: false,
   freezeTableName: true
 });
 
+Usuario.hasMany(Pedidos); // Associação entre Usuario e Pedidos
 
-usuario.sync();
+module.exports = Usuario;
 
-module.exports = usuario;
