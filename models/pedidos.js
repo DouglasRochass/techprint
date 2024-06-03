ad6e56b8ed314cbc1ad5468b8799bb0142f88aae
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2024-06-03 16:29:51 -0300

    Update pedidos.js

--- models/pedidos.js
+++ models/pedidos.js
@@ -1,9 +1,49 @@
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../db/conexao');
+// const Usuario = require('./usuarios');
+
+
+// const pedidos = sequelize.define('pedidos', {
+//   nome_pedido: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   data: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   descri: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   tempo_impre: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//  user_id: { 
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//     references: {
+//       model: Usuario, // Referência ao modelo Usuario
+//       key: 'id'       // Chave primária do modelo Usuario
+//     }
+//   }
+// }, {
+//   timestamps: false,
+//   freezeTableName: true
+// });
+
+// pedidos.sync();
+
+
+// module.exports = pedidos;
+
+
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db/conexao');
-const Usuario = require('./usuarios');
+const Usuario = require('./usuarios'); // Importe o modelo Usuario aqui
 
-
-const pedidos = sequelize.define('pedidos', {
+const Pedidos = sequelize.define('Pedidos', {
   nome_pedido: {
     type: DataTypes.STRING,
     allowNull: false
@@ -20,20 +60,16 @@ const pedidos = sequelize.define('pedidos', {
     type: DataTypes.STRING,
     allowNull: false
   },
- user_id: { 
+  user_id: { 
     type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Usuario, // Referência ao modelo Usuario
-      key: 'id'       // Chave primária do modelo Usuario
-    }
+    allowNull: false
   }
 }, {
   timestamps: false,
   freezeTableName: true
 });
 
-pedidos.sync();
+Pedidos.belongsTo(Usuario); // Associação entre Pedidos e Usuario
 
-
-module.exports = pedidos;
+module.exports = Pedidos;
+ 
