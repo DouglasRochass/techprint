2fe8afbeb54f5a54a34fbdc345cedd7611ff4a30
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2024-06-03 15:57:13 -0300

    Update usuarios.js

--- models/usuarios.js
+++ models/usuarios.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../db/conexao');
-const pedidos = require('./pedidos')
+const pedido = require('./pedidos')
 
 
 const usuario = sequelize.define('Usuario', {
@@ -32,7 +32,7 @@ const usuario = sequelize.define('Usuario', {
   freezeTableName: true
 });
 
-usuario.hasMany(pedidos)
+usuario.hasMany(pedido)
 usuario.sync();
 
 module.exports = usuario;
